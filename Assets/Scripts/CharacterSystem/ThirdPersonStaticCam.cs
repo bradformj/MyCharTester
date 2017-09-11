7fb51e6bcaf487544107331715bf19d5d8892d2b
bradformj <bradformj@users.noreply.github.com>
2017-09-11 10:16:09 -0400

    Added Standard Asset 3rd Person Controller
    Added and tuned the standard asset 3rd person controller to my liking.  Easy to add blockhead, working great.

---
+++ Assets/Scripts/CharacterSystem/ThirdPersonStaticCam.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonStaticCam : MonoBehaviour {
+
+    public Transform playerTransform;
+    public Camera playerCamera;
+    private Transform camTransform;
+    private Vector3 camRelativePosition;
+    private float camDefaultDistance = 10f;
+    public float camDistance;
+    public float zoomSpeed = 10f;
+    public float minDistance = 2f;
+    public float maxDistance = 20f;
+    private float scrollV;
+    private float angleH = 0;
+    private float angleV = 0;
+    
+
+
+	void Start () {
+
+        camTransform = playerCamera.GetComponent<Transform>();
+        camRelativePosition = new Vector3(0f, 7.5f, -10f);
+        camRelativePosition = (camRelativePosition / camRelativePosition.magnitude) * camDefaultDistance;
+        
+        camTransform.position = playerTransform.position + camRelativePosition;
+        camTransform.LookAt(playerTransform);
+
+        camDistance = camDefaultDistance;
+
+        angleH = camTransform.rotation.eulerAngles.y;
+        angleV = -camTransform.rotation.eulerAngles.x;
+
+    }
+	
+	void Update () {
+		
+        if((scrollV = Input.GetAxis("Mouse ScrollWheel")) != 0)
+        {
+            //zoom in and out
+            camDistance += scrollV * zoomSpeed;
+            if (camDistance < minDistance)
+            {
+                camDistance = minDistance;
+            } else if (camDistance > maxDistance) {
+                camDistance = maxDistance;
+            }
+            camRelativePosition = camTransform.position - playerTransform.position;
+        }
+
+        if(Input.GetMouseButton(0) || Input.GetMouseButton(1)) 
+        {
+            //pivot camera around player
+        }
+
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            //reset the camera view
+            camRelativePosition = new Vector3(0f, 7.5f, -10f);
+            camRelativePosition = (camRelativePosition / camRelativePosition.magnitude) * camDefaultDistance;
+            camTransform.position = playerTransform.position + camRelativePosition;
+            camTransform.LookAt(playerTransform);
+        }
+
+        camTransform.position = playerTransform.position + (camRelativePosition / camRelativePosition.magnitude) * camDistance;
+
+    }
+}
--- Assets/Scripts/StatSystem/RPGSystems/Scripts/Utilities/Databases/Interfaces/IDatabaseAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace RPGSystems.Utility.Database {
-    public interface IDatabaseAsset {
-        int Id { get; set; }
-        string Name { get; set; }
-    }
-}
